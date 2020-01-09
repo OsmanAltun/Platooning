201e7ee672e58403038c04dba9cfbbee80512612
Osman Altun <osman@145.137.EARLY-REGISTRATION.of.SURFnet.invalid>
2020-01-09 16:56:25 +0100

    post/get

--- database.py
+++ database.py
@@ -1,41 +1,15 @@
-import os
-import urllib.parse as up
-import psycopg2
+import requests
+import json
 
 
-def executeQuery(queryString):
-    up.uses_netloc.append("postgres")
-    url = up.urlparse(os.environ["DATABASE_URL"])
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
+def getData():
+    ship_api_url = "URL"
+    request_data = requests.get(ship_api_url)
+    return request_data.json()
 
-    cursor = conn.cursor()
-    cursor.execute(queryString)
-    conn.commit()
-
-    results = None
-    try:
-        results = cursor.fetchall()
-    except psycopg2.ProgrammingError:
-        results = "error"
-    return results
-
-def updateCar(carId, leftSpeed, rightSpeed, leftLineSensor, rightLineSensor, ultrasonicSensor):
-    executeQuery(f"""
-    UPDATE cars
-    SET leftSpeed={leftSpeed},
-        rightSpeed={rightSpeed},
-        leftLineSensor={leftLineSensor},
-        rightLineSensor={rightLineSensor},
-        ultrasonicSensor={ultrasonicSensor}
-    WHERE
-        carId = {carId}
-    """)
-
-def getAll():
-    return executeQuery("select * from cars")
+def postData(car):
+    url = 'URL'
+    payload = car
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    return r.status_code
